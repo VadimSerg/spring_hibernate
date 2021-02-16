eeeeee9c21ca402e4b01ee60d578a390443817bb
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2021-02-16 21:35:16 +1000

    commit

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -16,9 +16,6 @@ public class Car {
     @Column(name = "series")
     private int series;
 
-    @OneToOne(mappedBy ="car",cascade = CascadeType.ALL)
-    private User user;
-
 
     public Car() {
     }
@@ -33,31 +30,28 @@ public class Car {
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getModel() {
+
         return model;
     }
 
     public void setModel(String model) {
+
         this.model = model;
     }
 
     public int getSeries() {
+
         return series;
     }
 
     public void setSeries(int series) {
+
         this.series = series;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 
     @Override
